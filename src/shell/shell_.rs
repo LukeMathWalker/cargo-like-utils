32d14316ca1d996d29ba8b08e862a5408811f194
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2025-01-31 14:33:15 +0100

    fix: Adjust to windows-sys upgrade

--- src/shell/shell_.rs
+++ src/shell/shell_.rs
@@ -582,7 +582,7 @@ mod imp {
                 ptr::null_mut(),
                 OPEN_EXISTING,
                 0,
-                0,
+                std::ptr::null_mut(),
             );
             if h == INVALID_HANDLE_VALUE {
                 return TtyWidth::NoTty;
